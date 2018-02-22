c67efdf99c69e0963954e78a4ba3702037fe2d94
LeidyRosmery <rosmery.maldonado89@gmail.com>
2018-02-22 16:47:44 -0500

    mobile final

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,82 @@
 var widthWindows = $(window).width();
-$('.enlace-categoria').on('click', function(e) {
+$('.categoria').on('click', function(e) {
+  $('.nombre-categoria span').removeClass('glyphicon-menu-up');
+  $('.nombre-categoria span').addClass('glyphicon-menu-down');
   $('.menu').css('display', 'none');
-  // $('.menu').removeClass('mostrar');
-  // $(this).next().addClass('mostrar');
   $('.nombre-categoria').removeClass('active');
   e.preventDefault();
   $(this).next().toggleClass('mostrar', function(e) {
-      if ($(this).is(".mostrar")) {
+    if ($(this).is(".mostrar")) {
       $(this).prev().find('.nombre-categoria').addClass('active');
+      $(this).prev().find('.nombre-categoria span').removeClass('glyphicon-menu-down');
+      $(this).prev().find('.nombre-categoria span').addClass('glyphicon-menu-up');
       $(this).fadeIn();
     } else {
-    $(this).prev().find('.nombre-categoria').removeClass('active');
-      $(this).fadeOut();
+      $(this).prev().find('.nombre-categoria').removeClass('active');
+      $(this).prev().find('.nombre-categoria span').removeClass('glyphicon-menu-up');
+      $(this).prev().find('.nombre-categoria span').addClass('glyphicon-menu-down');
     }
   });
-  // $(this).find('.nombre-categoria').toggleClass('active');
 });
+var init = false;
+$("#more").on('click', function() {
+  console.log(init);
+  if (init) {
+    $('#more').html('CARGAR MAS VIDEOS<span class="arrow arrow-video arrow-categoria glyphicon glyphicon-menu-down"></span>');
+    $('.div-video').removeClass('fadeInRight');
+    $('.div-video').addClass('fadeOutRight');
+    init = false;
+  } else {
+    $('.div-video').addClass('fadeInRight');
+    $('.div-video').removeClass('fadeOutRight');
+    $('#more').html('CARGAR MENOS VIDEOS<span class="arrow arrow-video arrow-categoria glyphicon glyphicon-menu-up"></span>');
+    init = true;
+  }
+  $('.div-video').toggleClass('hidden-video', function(e) {});
+
+});
+
 
 $(document).ready(function() {
   if (widthWindows > 992) {
+    $('.menu').removeClass('fadeInDown');
+      $('.menu').addClass('fadeInLeft');
     $('#carrusel-banner .owl-dot').empty();
+    $('.link-banner').on('click', function(e) {
+      e.preventDefault();
+    });
+    $('.link-video-tip .div-play').attr('src', '/assets/img/video-tip/play_blanco.svg');
+    $('.link-video-tip').on('click', function(e) {
+      $('.link-video-tip').css('border-top', '3px transparent solid');
+      $('.link-video-tip').css('background-color', 'background-color: rgba(0, 0, 0, 0.4)');
+      $(this).css('border-top', '3px blue solid');
+      $(this).css('background-color', 'rgba(0, 0, 0, 0.1)');
+      var www = this.href;
+      console.log(www);
+      var f = function() {
+        window.location.href = www
+      };
+      setTimeout(f, 1500);
+    })
+  } else {
+    if (widthWindows <= 992) {
+      //$('.slider-nav').slick('unslick');
+    }
+  }
+});
+$(window).resize(function() {
+  if (widthWindows > 992) {
+    $('.link-video-tip').on('click', function(e) {
+      e.preventDefault();
+      $('.link-video-tip').css('border-top', '3px transparent solid');
+      $('.link-video-tip').css('background-color', 'background-color: rgba(0, 0, 0, 0.4)');
+      $(this).css('border-top', '3px blue solid');
+      $(this).css('background-color', 'rgba(0, 0, 0, 0.1)');
+
+    })
+  } else {
+    if (widthWindows <= 992) {
+      //$('.slider-nav').slick('unslick');
+    }
   }
 });
--- assets/js/carrusel.js
+++ assets/js/carrusel.js
@@ -1,28 +1,61 @@
   $(document).ready(function() {
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    animateOut: 'fadeOut',
-    animateIn: 'fadeIn',
-    // dotClass:'dots-banner',
-    smartSpeed: 450,
-    margin: 10,
-    dots:true,
-    loop:true,
-    navClass:['prev-banner','next-banner'],
-    nav: true,
-    items:1,
-    responsive:{
-    0:{
-        nav:false,
-    },
-    600:{
-        nav:false,
-        loop:true
-    },
-    992:{
-        nav:true,
-    }
-},
-    navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;stroke: #000;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-  }).on('changed.owl.carousel');
+$('.slider-drag1').slick({
+ slidesToShow: 1,
+ slidesToScroll: 1,
+ dots: true,
+ fade: true,
+ cssEase: 'linear',
+ arrows: true,
+     swipe: false,
+ autoplaySpeed: 8000,
+ autoplay: true,
+ vertical: false,
+ infinite: true,
+ responsive: [{
+     breakpoint: 992,
+     settings: {
+       slidesToShow: 1,
+       autoplay: true,
+       swipe: true,
+       arrows:false,
+     }
+   },
+   {
+     breakpoint: 480,
+     settings: {
+       slidesToShow: 1,
+       arrows: false,
+       dots: true,
+       autoplay: true,
+       swipe: true,
+     }
+   }
+ ]
+});
+
+    $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      cssEase: 'linear',
+      swipe: true,
+      asNavFor: '.slider-nav',
+      // autoplay: true
+    });
+
+    $('.slider-nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      asNavFor: '.slider-for',
+      dots: false,
+      arrows:true,
+      centerMode: true,
+      focusOnSelect: true,
+      responsive: [{
+        breakpoint: 992,
+        settings: "unslick"
+      }]
+    });
+
   })
